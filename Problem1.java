012d7e3574b6a6a68f0138ee102862a54089bc3b
Kido <229385941+Kido-wawa@users.noreply.github.com>
2025-09-11 21:06:20 +0000

    complete 1.6

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,21 @@
+public class Problem1
+{
+  public static void main(String[] args)
+  {
 
+int k = 5;
+k = k + 1;
+System.out.println("number is now" + K);
+k = k + 1;
+System.out.println("number is now" + K);
+k++;
+System.out.println("number is now" + K);
+k--;
+System.out.println("number is now" + K);
+k--;
+System.out.println("number is now" + K);
+k--;
+System.out.println("number is now" + K);
+
+  }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,13 @@
-
+public class Problem2
+{
+    public static void main(String[] args)
+    {
+        int kid = 858;
+        System.out.pritln(kid % 10);
+        kid /= 10;
+        System.out.println(kid % 10);
+        kid /= 10;
+        System.out.println(kid % 10);
+        kid /= 10;
+    }
+}
